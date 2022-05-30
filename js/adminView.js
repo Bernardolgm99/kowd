95b1f71a040d348651ceba33eb789d6a9a7e5202
Bernardolgm99 <bernardolgm99@gmail.com>
2022-05-31 00:50:45 +0100

    starting admin

---
+++ js/adminView.js
@@ -0,0 +1,90 @@
+let arrayModules = ['Basic', 'Medium', 'Hard'];
+let arrayLessons = ['Variable', 'Loop', 'Condition'];
+
+
+//CREATE DROPDOWN LIST
+
+arrayModules.forEach(module =>{
+    document.querySelector('#drop_lesson').innerHTML += `<li value="${module}"><a class="dropdown-item" href="#">${module}</a></li>`
+})
+
+/* Create catch error when the target don't have "className" */
+document.querySelector('#btn_module').addEventListener('focusout', (event) => {
+    if (event.relatedTarget.className === "dropdown-item"){
+        document.querySelector('#btn_module').innerHTML = event.relatedTarget.innerHTML
+    }
+})
+
+arrayLessons.forEach(lesson =>{
+    document.querySelector('#drop_exercises').innerHTML += `<li value="${lesson}"><a class="dropdown-item" href="#">${lesson}</a></li>`
+})
+
+document.querySelector('#btn_lesson').addEventListener('focusout', (event) => {
+    if (event.relatedTarget.className === "dropdown-item"){
+        document.querySelector('#btn_lesson').innerHTML = event.relatedTarget.innerHTML
+    }
+})
+
+
+
+
+
+
+
+
+
+document.querySelector('#typeExercise1').addEventListener('click', () => {
+    document.querySelector('#form').innerHTML = `
+    <div class="mb-3" id="v-pills-tab">
+        <div class="input-group mb-3">
+            <span class="input-group-text">Answer</span>
+            <input type="text" aria-label="" class="form-control">
+        </div>
+        <div class="input-group" id="typeExercise1Form">
+            <span class="input-group-text">Options</span>
+            <input type="text" aria-label="" class="form-control">
+            <input type="text" aria-label="" class="form-control">
+            <input type="text" aria-label="" class="form-control">
+            <input type="text" aria-label="" class="form-control">
+        </div>
+    </div>
+    <button class="btn btn-primary" id="typeExercise1" type="button">Send</button>
+    `
+})
+
+document.querySelector('#typeExercise2').addEventListener('click', () => {
+    document.querySelector('#form').innerHTML = `
+    <div class="mb-3">
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+            <label class="form-check-label" for="flexRadioDefault1">
+                True
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+            <label class="form-check-label" for="flexRadioDefault2">
+                False
+            </label>
+        </div>
+    </div>
+    <button class="btn btn-primary" id="typeExercise1" type="button">Send</button>
+    `
+})
+
+document.querySelector('#typeExercise3').addEventListener('click', () => {
+    document.querySelector('#form').innerHTML = `
+    <div class="mb-3">
+        <div class="input-group mb-3">
+            <span class="input-group-text">Question</span>
+            <textarea class="form-control" aria-label="With textarea"></textarea>
+        </div>
+        <div class="input-group mb-1">
+            <span class="input-group-text">Answer</span>
+            <input type="text" aria-label="" class="form-control">
+        </div>
+        <p class="text-secondary">Please, put a comma between answers</p>
+    </div>
+    <button class="btn btn-primary" id="typeExercise1" type="button">Send</button>
+    `
+})
