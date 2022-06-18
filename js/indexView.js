95262f0724ffa6a491659834cb246c9570e9c6e7
Bernardolgm99 <bernardolgm99@gmail.com>
2022-06-18 15:45:42 +0100

    landing page

--- js/indexView.js
+++ js/indexView.js
@@ -10,4 +10,9 @@ function typeWriter(element){
 
 let title = document.querySelector('.type-writer')
 
+console.log(title.parentNode);
+console.log(title.parentNode.offsetHeight);
+
+title.parentNode.height = title.parentNode.offsetHeight;
+
 typeWriter(title)
