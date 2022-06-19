f9191a95a5a456efaf5e0cc0dc2395810d1f4a0b
Eduardo Alves <edualves884@gmail.com>
2022-06-19 01:08:16 +0100

    alterações finais e js a funcionar sem tar conectado com o resto do codigo

--- js/models/userModel.js
+++ js/models/userModel.js
@@ -1,35 +1,34 @@
 let usersData = JSON.parse(localStorage.users)
- 
+
 //REGISTER USER
 export function register(first_name, last_name, email, password, confirm_password) {
+
     let newEmail = usersData.find(user => user.email == email)
-    
     
     //VERIFICATION TO SEE IF THE PASSWORDS MATCH WITH EACH OTHER
     if((password == confirm_password) && (newEmail == undefined)) {
-        /*let validation = validateEmail(email)
-        if(validation == true) {*/
-            usersData.push(new User(first_name, last_name, password, email, 0))
+        if(validateEmail(email) == true) {
+            usersData.push(new User(first_name, last_name, password, email))
+            console.log(usersData)
             localStorage.setItem('users', JSON.stringify(usersData))
-            window.location.href = "./login.html"
+            return 
         } else {
             throw Error('Invalid Email Address')
         }
-    /*} else {
+    } else {
         throw Error('Something went wrong, try again.')
-    }*/
+    }
 }
 
 //EMAIL VERIFICATION
-/*function validateEmail(email) {
-    console.log(email)
-    console.log(email.match(/^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/));
-    if(email.match(/^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/))
+function validateEmail(email) {
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+    if(email.match(pattern))
   {
     return (true)
   }
     return (false)
-}*/
+}
 
 
 //LOGIN USER
@@ -39,8 +38,8 @@ export function login(email, password){
     let loggedUser = usersData.find(user => user.email == email && user.password == password)
 
     if(loggedUser != undefined){
-        alert('welcome back!')
         localStorage.setItem('currentUser', JSON.stringify(loggedUser))
+        return (loggedUser)
     } else {
         throw Error('Email or password are invalid! Please try again')
     }
@@ -66,9 +65,9 @@ export default class User{
     level = 0
     point = 0
     img= "../media/userIcon/user1.jpg"
-    constructor(first_name,last_name, password, email, type){
+    constructor(first_name, last_name, password, email, type = 0){
         this.id = (
-            usersData.length = 0 ? 1 : usersData[usersData.length - 1].id + 1)
+            usersData.length == 0 ? 1 : usersData[usersData.length - 1].id + 1)
         this.first_name = first_name
         this.last_name = last_name
         this.password = password
